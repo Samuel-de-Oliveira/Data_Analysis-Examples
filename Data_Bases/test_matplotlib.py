aeea1774bab39681927922043670a7b9fdc96c69
Samuel-de-Oliveira <samwolfg13@gmail.com>
2024-10-16 17:52:23 -0300

    Add a new graphic

---
+++ Data_Bases/test_matplotlib.py
@@ -0,0 +1,10 @@
+import matplotlib.pyplot as plt
+
+# f(x) = 2x
+x   = [1, 2, 3, 4]
+f_x = [2, 4, 6, 8]
+
+if __name__ == "__name__":
+    plt.title("Function: f(x) = 2x")
+    plt.plot(x, f_x)
+    plt.show
